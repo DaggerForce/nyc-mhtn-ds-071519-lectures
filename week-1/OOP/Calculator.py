02b3a0a4163885bb326f6df42c7126ea5934c1fc
Gal Gilor <gilorgal2010@gmail.com>
2019-07-18 14:10:00 -0400

    rump row

---
+++ week-1/OOP/Calculator.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[133]:
+
+
+class Calculator:
+    def __init__(self, data):
+        import math
+        self.data = data
+        self.length = len(data) #self.data
+        self.__update_stats()
+    
+    def __update_stats(self):  
+        self.mean = self.calc_mean()
+        self.median = self.calc_median()
+        self.variance = self.calc_var()
+        self.standev = self.calc_std()
+        
+    def calc_mean(self):
+        n = self.length
+        total = sum(self.data)
+        return total/n
+    
+    def calc_median(self):
+        self.data.sort()
+        n = int(self.length)
+        # if the list of numbers has odd numbers
+        median = self.data[round(n/2)]
+        # if the list has an even number
+        if n % 2 == 0:
+            down = self.data[int(n/2-1)]
+            up = self.data[int(n/2)]
+            median = (up + down)/2
+        return median
+    
+    def calc_var(self):
+        # get the mean
+        n = self.length
+        mean = self.mean
+        # substract each value from the mean and raise the value power of 2
+        variance  = list(map(lambda x: (x-mean)**2/(n-1) ,self.data))
+        var_sum = sum(variance)
+        return var_sum
+    
+    def calc_std(self): 
+        return self.variance**0.5
+    
+    def add_data(self, numbers):
+        if type(numbers) == list:
+            self.data.extend(numbers)
+        else:
+            self.data.append(numbers)
+        self.__update_stats()
+        return
+    
+    def remove_data(self, index):
+        del self.data[index]
+        self.__update_stats()
+        return 
+        
+
+
+# In[135]:
+
+
+calc = Calculator([1, 2, 3, 4])
+print(calc.mean)
+calc.remove_data(2)
+print(calc.mean)
+calc.data
+
+
+# In[ ]:
+
+
+
+
